0025cf4e77788f405ae2a37712c7af921ef85005
Ghabryel Henrique <ghabryelhenrique@hotmail.com>
2024-06-05 19:12:01 -0300

    Adição de novos modelos de IA e ajustes com novas metricas

--- src/modelo_ia.py
+++ src/modelo_ia.py
@@ -1,14 +1,21 @@
 import numpy as np
 import gridfs
+import os
+import io
+import pandas as pd
+
 from pymongo import MongoClient
 from PIL import Image
-import io
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import pandas as pd
 from dotenv import load_dotenv
-import os
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import Accuracy, Precision, Recall, AUC
 
 def conectar_mongodb():
     print('Conectando ao mongo')
@@ -58,7 +65,22 @@ def carregar_imagens_do_mongodb(db, fs, labels_dict, lista_imagens):
 
     return np.array(imagens), np.array(labels)
 
-def construir_modelo():
+def calcular_metricas(y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+    tn, fp, fn, tp = cm.ravel()
+    accuracy = accuracy_score(y_true, y_pred)
+    prevalence = (tp + fn) / (tp + fn + tn + fp)
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+    
+    return {
+        'accuracy': accuracy,
+        'prevalence': prevalence,
+        'sensitivity': sensitivity,
+        'specificity': specificity
+    }
+
+def construir_modelo_cnn(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']):
     print('Construindo Modelo')
     modelo = Sequential([
         Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
@@ -67,19 +89,46 @@ def construir_modelo():
         MaxPooling2D(2, 2),
         Flatten(),
         Dense(128, activation='relu'),
-        Dense(20, activation='softmax')  # Ajuste aqui para 20 classes
+        Dense(20, activation='softmax')
     ])
-    modelo.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])  # Ajuste aqui para categorical_crossentropy
+    
+    modelo.compile(optimizer=optimizer, loss=loss, metrics=metrics)
     return modelo
 
-def treinar_modelo(modelo, imagens, labels):
+def construir_modelo_knn():
+    print('Construindo Modelo KNN')
+    modelo = KNeighborsClassifier(n_neighbors=5)
+    return modelo
+
+def construir_modelo_random_forest():
+    print('Construindo Modelo Random Forest')
+    modelo = RandomForestClassifier(n_estimators=100, random_state=42)
+    return modelo
+
+def treinar_modelo_knn(modelo, imagens, labels):
+    print('Treinando Modelo KNN')
+    imagens_flat = imagens.reshape(imagens.shape[0], -1)
+    modelo.fit(imagens_flat, labels)
+    y_pred = modelo.predict(imagens_flat)
+    metricas = calcular_metricas(labels, y_pred)
+    return modelo, metricas
+
+def treinar_modelo_random_forest(modelo, imagens, labels):
+    print('Treinando Modelo Random Forest')
+    imagens_flat = imagens.reshape(imagens.shape[0], -1)
+    modelo.fit(imagens_flat, labels)
+    y_pred = modelo.predict(imagens_flat)
+    metricas = calcular_metricas(labels, y_pred)
+    return modelo, metricas
+
+def treinar_modelo_cnn(modelo, imagens, labels):
     print('Treinando Modelo')
     if imagens.size == 0 or labels.size == 0:
         raise ValueError("Imagens ou labels estão vazios. Verifique se os dados foram carregados corretamente.")
     
     datagen = ImageDataGenerator(rescale=1.0/255.0)
     train_generator = datagen.flow(imagens, labels, batch_size=32)
-    modelo.fit(train_generator, epochs=10)
+    modelo.fit(train_generator, epochs=1000)
     return modelo
 
 def processar_imagens(caminho_csv, caminho_txt):
@@ -88,18 +137,35 @@ def processar_imagens(caminho_csv, caminho_txt):
     labels_dict = carregar_labels_do_csv(caminho_csv)
     lista_imagens = carregar_lista_imagens(caminho_txt)
     imagens, labels = carregar_imagens_do_mongodb(db, fs, labels_dict, lista_imagens)
-    
+
+
     if imagens.size == 0 or labels.size == 0:
         raise ValueError("Nenhuma imagem ou label foi carregada. Verifique os caminhos e os dados fornecidos.")
     
-    modelo = construir_modelo()
-    modelo_treinado = treinar_modelo(modelo, imagens, labels)
-    return modelo_treinado
+    optimizer = Adam(learning_rate=0.001)
+    loss = CategoricalCrossentropy()
+    metrics = [Accuracy(), Precision(), Recall(), AUC()]
+    modelo_cnn = construir_modelo_cnn(optimizer=optimizer, loss=loss, metrics=metrics)
+    modelo_cnn_treinado = treinar_modelo_cnn(modelo_cnn, imagens, labels)
+    
+    modelo_knn = construir_modelo_knn()
+    modelo_knn_treinado, metricas_knn = treinar_modelo_knn(modelo_knn, imagens, labels)
+    print("Métricas KNN:", metricas_knn)
+    
+    modelo_rf = construir_modelo_random_forest()
+    modelo_rf_treinado, metricas_rf = treinar_modelo_random_forest(modelo_rf, imagens, labels)
+    print("Métricas Random Forest:", metricas_rf)
+    
+    return modelo_cnn_treinado, modelo_knn_treinado, modelo_rf_treinado
 
 if __name__ == '__main__':
     load_dotenv()
-
+    
     caminho_csv = os.getenv('CAMINHO_CSV_LABELS')
     caminho_txt = os.getenv('CAMINHO_TXT')
-    modelo_treinado = processar_imagens(caminho_csv, caminho_txt)
-    modelo_treinado.save('modelo_treinado.h5')
+
+    modelo_cnn_treinado, modelo_knn_treinado, modelo_rf_treinado = processar_imagens(caminho_csv, caminho_txt)
+    
+    modelo_rf_treinado.save('modelo_rf_treinado.h5')
+    modelo_knn_treinado.save('modelo_knn_treinado.h5')
+    modelo_cnn_treinado.save('modelo_cnn_treinado.h5')
